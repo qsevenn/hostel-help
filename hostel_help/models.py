05f681035f53e2d258897544d36deb19ac42a2d6
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-11-14 18:07:44 +0200

    report to database connection, readress to profile

--- hostel_help/models.py
+++ hostel_help/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    title = models.CharField(max_length=70)
+    problem_type =  models.CharField(max_length=70)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} {self.date}"
--- hostel_help/views.py
+++ hostel_help/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Report
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, radyna!")
+    return render(request, 'index.html')
 
 
 def register(request):
@@ -16,7 +17,15 @@ def login(request):
 
 
 def profile(request):
-    return render(request, 'profile.html')
+    if request.method == "POST":
+        if request.POST.get('title') and request.POST.get('problem_type') and request.POST.get('description'):
+            new_report = Report()
+            new_report.title = request.POST.get('title')
+            new_report.problem_type = request.POST.get('problem_type')
+            new_report.description = request.POST.get('description')
+            new_report.save()
+            return render(request, 'profile.html')
+    return render(request, 'report-problem.html')
 
 def report(request):
     return render(request, 'report-problem.html')
