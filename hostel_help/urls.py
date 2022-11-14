89273aeaba49e5a8cef997dbc5180ac8e72fd6cf
syniukdima <105307746+syniukdima@users.noreply.github.com>
2022-11-14 16:14:24 +0200

    routing

--- hostel_help/urls.py
+++ hostel_help/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('report/', views.report, name='report'),
 ]
--- hostel_help/views.py
+++ hostel_help/views.py
@@ -5,3 +5,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, radyna!")
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def report(request):
+    return render(request, 'report-problem.html')
