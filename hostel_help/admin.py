4bc2deedf55a23a2125547ac5338d107b4823b0d
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-11-16 00:24:59 +0200

    disabled changing of reports for admin

--- hostel_help/admin.py
+++ hostel_help/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from hostel_help.models import Report
+
 # Register your models here.
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    actions = ['delete', 'view']
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+        # return super().has_change_permission(request, obj=obj)
+
+
+
