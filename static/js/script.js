2c9c77412d453628f7350d89e62f226d811ea189
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-12-08 17:30:19 +0200

    show/hide answers

--- static/js/script.js
+++ static/js/script.js
@@ -18,16 +18,48 @@ function showAnswerPopup(obj) {
     answer_form.style.display = "block";
 }
 
-function showReplies(){
-    document.getElementById("all-replies").style.display = "block";
-    document.getElementById("show-replies").style.display = "none";
-    document.getElementById("hide-replies").style.display = "block";
+function showReplies(obj){
+    var report_id = obj.getAttribute("data-value");
+    var elements = document.getElementsByClassName("all-replies");
+    elements = Array.prototype.slice.call(elements)
+    // show_buttons = document.getElementsByClassName("show-replies");
+    var hide_buttons = document.getElementsByClassName("hide-replies");
+    hide_buttons = Array.prototype.slice.call(hide_buttons);
+    console.log(elements)
+    elements.forEach((element) => {
+        if (report_id === element.getAttribute("data-value")) {
+            console.log(element.getAttribute("data-value"));
+            element.style.display = "block";
+            obj.style.display = "none"
+            hide_buttons.forEach((button) => {
+                if (report_id === button.getAttribute("data-value")){
+                    button.style.display = "block";
+                }
+            })
+        }
+    })
 }
 
-function hideReplies() {
-  document.getElementById("all-replies").style.display = "none";
-  document.getElementById("hide-replies").style.display = "none";
-  document.getElementById("show-replies").style.display = "block";
+function hideReplies(obj) {
+    var report_id = obj.getAttribute("data-value");
+    var elements = document.getElementsByClassName("all-replies");
+    elements = Array.prototype.slice.call(elements);
+    show_buttons = document.getElementsByClassName("show-replies");
+    // var hide_buttons = document.getElementsByClassName("hide-replies");
+    show_buttons = Array.prototype.slice.call(show_buttons);
+    // console.log(elements);
+    elements.forEach((element) => {
+      if (report_id === element.getAttribute("data-value")) {
+        // console.log(element.getAttribute("data-value"));
+        element.style.display = "none";
+        obj.style.display = "none";
+        show_buttons.forEach((button) => {
+          if (report_id === button.getAttribute("data-value")) {
+            button.style.display = "block";
+          }
+        });
+      }
+    });
 }
 
 function hideAnswerPopup() {
