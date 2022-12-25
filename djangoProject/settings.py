1cd9a079b4de1197425f8ed2f6fdc1a7a297e6aa
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-12-25 10:48:29 +0200

    hosts

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a=5k)pwt47s3!$s5o6k+*nj%@xf-@t+ne(6i6mw=3wv+4_l6=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
