66a435754e1e900ee6aa82218d0c9b3ebf27fe0f
Anton Proskura <37810633+qsevenn@users.noreply.github.com>
2023-08-04 09:01:20 +0300

    Turn off debug

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a=5k)pwt47s3!$s5o6k+*nj%@xf-@t+ne(6i6mw=3wv+4_l6=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
